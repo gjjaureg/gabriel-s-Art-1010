84433b25f6c0c8333bf437887db3b3cfc5500586
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2024-04-23 23:42:27 -0700

    lab4 changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,38 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Rock, Paper, Scissors
+// An implementation of the game
+// Gabriel Jauregui <gjjaureg@ucsc.edu>
+// April 2024
+
+//Baking sourdough bread
+
+//Feed sourdough starter 4-8 hours before baking
+//Mix dough
+  //Mix water and sourdough starter until dissolved
+  //Add bread flour
+  //Mix ingredients together until fully absorbed
+//Let dough rest in a covered bowl for 30 minutes
+//After 30 minutes add salt and knead the dough
+//Recover the bread in it’s bowl and let the dough rise
+//Stretch and fold the dough in 30-90 minute increments to create air pockets
+  //Note: amount of time needed for dough to rise may vary
+//Shape dough after it has risen
+//Once dough is shaped, put it in a floured banneton or bowl covered with a floured kitchen towel, cover, and let proof in fridge for 8-12 hours
+//Once proofed, place in a dutch oven preheated to 450-500 degrees
+//Bake in a 450-500 degree oven for 20 minutes, before lowering the temperature to 400 and baking for 20-40 minutes until golden brown on the outside
+//Let rest until completely cooled, 1-2 hours
+
+//Rock paper scissors
+
+//Set rock, paper, and scissors as variables
+//Set rock to beat scissors
+//Set paper to beat rock
+//Set scissors to beat paper
+//Set 2 of the same to be a draw
+//Choose computer’s move
+//Accept user input
+//Validate user move
+//Test win/lose/draw condition
+
 
 // Constants
 
