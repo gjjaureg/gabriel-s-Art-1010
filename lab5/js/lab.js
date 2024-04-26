dd9e66325f9c3033a1aed9d6a154827c950135cc
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2024-04-25 18:28:04 -0700

    lab5-1

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,10 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - to declare the variables from task 1
+// Author: Gabriel Jauregui
+// Date:4/25/2024
 
 // Constants
 
 // Functions
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995
+
+age = 2024 - year;
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "<br>");
+
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
