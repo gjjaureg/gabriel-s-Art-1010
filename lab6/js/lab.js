a57586aa76a8d5df57ac8c2a8d4f27e350bfb9ba
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2024-04-27 22:35:56 -0700

    lab 6 edits

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - to identify declare myTransport and object myMainRide
+// Author: Gabriel Jauregui
+// Date:4/27/2024
 
 // Constants
+const myTransport = ['bus' ,'car', 'walking'];
 
-// Functions
+let myMainRide = {
+  make: "Ford",
+  model: "Taurus",
+  color: "rusty",
+  year: 1995,
+  age: function () {
+      return 2025 - this.year;
+  }
+}
+document.writeln("Kinds of transportation I use ", myTransport, "</br>");
+
+document.writeln("My Main Ride: <pre>", 
+JSON.stringify(myMainRide, null, '\t'), "</pre>");
+
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
