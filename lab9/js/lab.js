9d7cc312b0c6b054d3c1dfe68f0313e90271369a
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2024-05-12 19:01:28 -0700

    lab9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,27 +1,11 @@
-// index.js - to create and use anonymous functions and callbacks
+// index.js - to utilize jQuery to make a special button
 // Author: Gabriel Jauregui
-// Date:5/4/2024
+// Date:5/12/2024
 
-//create a function that uses math
-function MathMech(x){
-    var result = x + x;
-    return result
-}
-//create an array of numbers
-const mathArray = [ 3, 6, 9]
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
 
-//use .map to use function on array
-mathArray.map(function(x){
-    return x - 2;
-})
+console.log("script run");
 
-//use and anonymous function
-mathArray.map(function(x){
-    return x - 2;
-})
-
-//sets a variable to map function
-let primaMath = mathArray.map(MathMech);
-console.log("results: ", primaMath)
-
-
+$(document).on('click', '#challenge', function(){
+    $("#challenge").toggleClass("special");
+});
