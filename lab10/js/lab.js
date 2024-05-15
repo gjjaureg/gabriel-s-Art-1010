a443c34c8224aa921a4aa13f38a0910ea99a6e21
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2024-05-14 21:29:27 -0700

    lab 10

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -1,11 +1,27 @@
-// index.js - to utilize jQuery to make a special button
-// Author: Gabriel Jauregui
-// Date:5/12/2024
+/*
+   lab.js - This simple JavaScript/jQuery script appends new elements to an output div
 
-$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+   Requirements: jQuery must be loaded for this script to work.
 
-console.log("script run");
+   Author: Wes modes
+   Date: 2023
+*/
 
-$(document).on('click', '#challenge', function(){
-    $("#challenge").toggleClass("special");
+function generateRandomText() {
+    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+    const min = 3;
+    const max = 100;
+    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
+    // Get a random starting index to slice the Lorem Ipsum text
+    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
+    // Generate the random Lorem Ipsum-like text
+    return text.slice(randStart, randStart + randLen);
+  }
+
+// click listener for button
+$("#make-convo").click(function(){
+    // get new fake dialogue
+    const newText = generateRandomText();
+    // append a new div to our output div
+    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
 });
