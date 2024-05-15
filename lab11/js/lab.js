1810ffcbb8e5810aee7492898105dd281a220509
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2024-05-15 16:26:51 -0700

    lab11

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -7,21 +7,20 @@
    Date: 2023
 */
 
-function generateRandomText() {
-    const text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-    const min = 3;
-    const max = 100;
-    const randLen = Math.floor(Math.random() * (max - min + 1)) + min;
-    // Get a random starting index to slice the Lorem Ipsum text
-    const randStart = Math.floor(Math.random() * (text.length - randLen + 1));
-    // Generate the random Lorem Ipsum-like text
-    return text.slice(randStart, randStart + randLen);
-  }
-
+function sortString(name){
+  //splits the string
+  var nameSplit = name.split('');
+  //sorts every letter in the splitted string
+  var sortSplit = nameSplit.sort();
+  //returns sorted name and joins the letter together 
+  return sortSplit.join('');
+}
 // click listener for button
-$("#make-convo").click(function(){
-    // get new fake dialogue
-    const newText = generateRandomText();
-    // append a new div to our output div
-    $("#output").append('<div class="text"><p>' + newText + '</p></div>');
+$("#submit").click(function(){
+  // get value of input field
+  const userName = $("#user-name").val();
+  // now let's sort it
+  userNameSorted = sortString(userName);
+  // append a new div to our output div
+  $("#output").html('<div class="text"><p>' + userNameSorted + '</p></div>');
 });
